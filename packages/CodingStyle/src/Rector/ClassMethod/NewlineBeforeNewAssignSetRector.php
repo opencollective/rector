59594da0a8b487cd0109067d577bfc31b40b2902
Tomas Votruba <tomas.vot@gmail.com>
2019-07-14 15:08:56 +0200

    [CodingStyle] Add NewlineBeforeNewAssignSetRector

--- packages/CodingStyle/src/Rector/ClassMethod/NewlineBeforeNewAssignSetRector.php
+++ packages/CodingStyle/src/Rector/ClassMethod/NewlineBeforeNewAssignSetRector.php
@@ -3,6 +3,13 @@
 namespace Rector\CodingStyle\Rector\ClassMethod;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\FunctionLike;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Nop;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\Rector\AbstractRector;
 use Rector\RectorDefinition\CodeSample;
 use Rector\RectorDefinition\RectorDefinition;
@@ -54,15 +61,43 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [\PhpParser\Node\Stmt\ClassMethod::class];
+        return [FunctionLike::class];
     }
 
     /**
-     * @param \PhpParser\Node\Stmt\ClassMethod $node
+     * @param FunctionLike|Node\Stmt\ClassMethod|Node\Stmt\Function_|Node\Expr\Closure $node
      */
     public function refactor(Node $node): ?Node
     {
-        // change the node
+        $previousStmtVariableName = null;
+
+        foreach ((array) $node->stmts as $key => $stmt) {
+            $currentStmtVariableName = null;
+
+            /** @var Expression $originalStmt */
+            $originalStmt = $stmt;
+            if ($stmt instanceof Expression) {
+                $stmt = $stmt->expr;
+            }
+
+            if ($stmt instanceof Assign || $stmt instanceof MethodCall) {
+                if ($stmt->var instanceof Variable) {
+                    $currentStmtVariableName = $this->getName($stmt->var);
+                }
+            }
+
+            if ($previousStmtVariableName !== null && $currentStmtVariableName !== null) {
+                if ($previousStmtVariableName !== $currentStmtVariableName) {
+                    // insert newline before
+                    array_splice($node->stmts, $key, 0, [new Nop()]);
+
+                    // @todo remove left indent :) - in Nop print of node?
+                    $originalStmt->setAttribute(AttributeKey::ORIGINAL_NODE, null);
+                }
+            }
+
+            $previousStmtVariableName = $currentStmtVariableName;
+        }
 
         return $node;
     }
